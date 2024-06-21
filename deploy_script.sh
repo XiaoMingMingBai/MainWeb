a81a3b98fe0545f10df0d00b11c5f9b948257ca8
xiaomingmingbai <1269596930@qq.com>
2024-06-21 20:39:15 +0800

    update

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,11 @@
 # Navigate to the Nginx root directory where your Angular project is served
 cd /var/www/angular-app
 
-mkdir test
-
 # Remove old files
-# rm -rf *
+sudo rm -rf */
 
 # Copy new build files from the GitHub Actions workspace
-# cp -r ~/path/to/github/workspace/dist/angular-app/* .
+sudo cp -r ~/path/to/github/workspace/dist/angular-app/* .
 
 # Restart Nginx if necessary (optional)
-# sudo systemctl restart nginx
+sudo systemctl restart nginx
