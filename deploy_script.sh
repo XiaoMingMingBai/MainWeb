c0ef00460ec120d755b41bd54ef8e4c07808b314
xiaomingmingbai <1269596930@qq.com>
2024-06-21 21:41:30 +0800

    update

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,7 +5,6 @@ cd ~/app/angular-app
 rm -rf */
 
 # Copy new build files from the GitHub Actions workspace
-cp -r ~/path/to/github/workspace/dist/angular-app/* .
 
 # Restart Nginx if necessary (optional)
 # sudo systemctl restart nginx
