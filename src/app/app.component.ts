7a4559cf74fcd8bc4634d8b2fc22cc8c1e289c88
xiaomingmingbai <1269596930@qq.com>
2024-06-22 13:07:05 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,31 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { url } from 'inspector';
+import { abort } from 'process';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeroesComponent
-  ],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'MainWeb';
+  cardInfo = [
+    {
+      name: 'Owncloud',
+      info: 'Owncloud is a suite of client-server software for creating file hosting services and using them. ',
+      url: 'http://127.0.0.1:1212/',
+      image: '../assets/oc-logo-circle-kiteworks.svg',
+      about:
+        'Owncloud is a suite of client-server software for creating file hosting services and using them. Owncloud is functionally very similar to the widely used Dropbox, with the primary functional difference being that Owncloud is free and open-source, and thereby allowing anyone to install and operate it without charge on a private server.',
+    },
+    {
+      name: 'MyWeb',
+      info: 'This is a test website for the purpose of learning Angular.',
+      url: 'http://127.0.0.1:1213/',
+      about: 'This is test.',
+    },
+  ];
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [
-    MatCardModule,
-    MatButtonModule,
-    MatExpansionModule
-  ],
+  imports: [MatCardModule, MatButtonModule, MatExpansionModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  
+  @Input() heroesName = '';
+  @Input() heroesInfo = '';
+  @Input() heroesUrl = '';
+  @Input() heroesImage = '';
+  @Input() heroesAbout = '';
 }
