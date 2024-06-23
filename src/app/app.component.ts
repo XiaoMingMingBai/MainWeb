76c5c2d2247fa5ae71fdedc1a1b18161ca909ec1
xiaomingmingbai <1269596930@qq.com>
2024-06-23 12:56:11 +0800

    localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
     {
       name: 'Owncloud',
       info: 'Owncloud is a suite of client-server software for creating file hosting services and using them. ',
-      url: 'http://127.0.0.1:1212/',
+      url: 'http://localhost:1212/',
       image: '../assets/oc-logo-circle-kiteworks.svg',
       about:
         'Owncloud is a suite of client-server software for creating file hosting services and using them. Owncloud is functionally very similar to the widely used Dropbox, with the primary functional difference being that Owncloud is free and open-source, and thereby allowing anyone to install and operate it without charge on a private server.',
@@ -25,7 +25,7 @@ export class AppComponent {
     {
       name: 'MyWeb',
       info: 'This is a test website for the purpose of learning Angular.',
-      url: 'http://127.0.0.1:1213/',
+      url: 'http://localhost:1213/',
       about: 'This is test.',
     },
   ];
